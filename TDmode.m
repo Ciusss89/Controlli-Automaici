55d6c3b3d3577dad9c6ad9084b9daf1ba6344132
Ciusss89 <ciusss@gmail.com>
2013-06-23 13:29:42 +0200

    removed some files

--- TDmode.m
+++
@@ -1,9 +0,0 @@
-function [ risGcz,risLz] = TDmode( Ts,Glp,Gc,Ga,Gp,Gs,Gf)
-%TDMODE Discretizzazione controllore.
-    s=tf('s');
-    risGcz=c2d(Gc,Ts,'matched'); % discretizzazione Gc
-    Gczoh=1/(1+s*Ts/2);
-    risLz=Gczoh*Glp*Gc*Ga*Gp*Gs*Gf;
-
-end
-
